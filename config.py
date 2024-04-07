f02971734cd1fba9957816f3cd99b5f9325f305f
sukunasayzz <155884043+sukunasayzz@users.noreply.github.com>
2024-04-07 23:33:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,10 +35,10 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
 # ----------------------------------------------------------------
 
 # ----------------------------------------------------------------
-LOGGER_ID = int(getenv("LOGGER_ID", -1001919135283))
+LOGGER_ID = int(getenv("LOGGER_ID", --1002004619729))
 # ----------------------------------------------------------------
 # ----------------------------------------------------------------
-OWNER_ID = int(getenv("OWNER_ID", 6664582540))
+OWNER_ID = int(getenv("OWNER_ID", 6769798391)
 # -----------------------------------------------------------------
 # -----------------------------------------------------------------
 
@@ -67,8 +67,8 @@ GIT_TOKEN = getenv(
 
 # ------------------------------------------------------------------------
 # -------------------------------------------------------------------------
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ALLTYPECC")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DAXXSUPPORT")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/sexysoulnet")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/sexysoulnet")
 # ------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------
 
@@ -141,21 +141,21 @@ confirmer = {}
 # ------------------------------------------------------------------------
 # ------------------------------------------------------------------------
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/cfbdee8103102bcb2e5da.jpg"
+    "START_IMG_URL", "https://telegra.ph/file/89c87da88ccbcdf41c42d.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://telegra.ph/file/00360393a15daf7fc4e9d.jpg"
+    "PING_IMG_URL", "https://telegra.ph/file/89c87da88ccbcdf41c42d.jpg"
 )
-PLAYLIST_IMG_URL = "https://telegra.ph/file/d723f4c80da157fca1678.jpg"
-STATS_IMG_URL = "https://telegra.ph/file/d30d11c4365c025c25e3e.jpg"
-TELEGRAM_AUDIO_URL = "https://telegra.ph/file/48f39202823b358203234.jpg"
-TELEGRAM_VIDEO_URL = "https://telegra.ph/file/e575ae40d6635250974e1.jpg"
-STREAM_IMG_URL = "https://telegra.ph/file/03efec694e41e891b29dc.jpg"
-SOUNCLOUD_IMG_URL = "https://telegra.ph/file/d723f4c80da157fca1678.jpg"
-YOUTUBE_IMG_URL = "https://telegra.ph/file/4dc854f961cd3ce46899b.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://telegra.ph/file/d723f4c80da157fca1678.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://telegra.ph/file/6c741a6bc1e1663ac96fc.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://telegra.ph/file/6c741a6bc1e1663ac96fc.jpg"
+PLAYLIST_IMG_URL = "https://telegra.ph/file/89c87da88ccbcdf41c42d.jpg"
+STATS_IMG_URL = "https://telegra.ph/file/89c87da88ccbcdf41c42d.jpg"
+TELEGRAM_AUDIO_URL = "https://telegra.ph/file/89c87da88ccbcdf41c42d.jpg"
+TELEGRAM_VIDEO_URL = "https://telegra.ph/file/89c87da88ccbcdf41c42d.jpg"
+STREAM_IMG_URL = "https://telegra.ph/file/89c87da88ccbcdf41c42d.jpg"
+SOUNCLOUD_IMG_URL = "https://telegra.ph/file/89c87da88ccbcdf41c42d.jpg"
+YOUTUBE_IMG_URL = "https://telegra.ph/file/89c87da88ccbcdf41c42d.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://telegra.ph/file/89c87da88ccbcdf41c42d.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://telegra.ph/file/89c87da88ccbcdf41c42d.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://telegra.ph/file/89c87da88ccbcdf41c42d.jpg"
 
 # ------------------------------------------------------------------------
 # ------------------------------------------------------------------------
